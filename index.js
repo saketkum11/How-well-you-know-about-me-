fa0f64173cc89160a4217109e1377e21b467ca80
Saket kumar <46194436+saketkum11@users.noreply.github.com>
2021-09-16 04:24:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+var readlineSync = require('readline-sync');
+const chalk = require('chalk');
+ var score = 0;
+var userName = readlineSync.question(chalk.blue('whats your name?'));
+ console.log("----------")
+
+
+console.log("Welcome! " + userName + ", Do you know Saket?");
+
+
+function play(question , answer){
+
+  var userAnswer = readlineSync.question(question);
+
+  if(userAnswer == answer){
+    console.log(chalk.green("right"));
+    score = score + 1;
+   
+   console.log("----------");
+  
+  }else{
+    console.log(chalk.red("wrong"));
+    console.log("----------");
+  }
+}
+
+
+
+
+
+var questions = [{
+
+  question : "where I do live ?",
+  answer : "Bihar"
+},
+
+ questionTwo = {
+
+  question : "From where I have done my Undergraduation ?",
+  answer : "BIT"
+},
+
+  questionThree = {
+  question : "Do you follow me in on any Social media?",
+  answer : "No"
+},
+ questionFour = {
+  question : "Do you know him from school or college time?",
+  answer : "No"
+
+},questionFive = {
+  question : "Which school I have studied in?",
+  answer : "Saint Teresa school"
+}
+
+];
+
+for( var i = 0 ; i < questions.length ; i++){
+  
+  var vote = questions[i];
+  play(vote.question, vote.answer);
+
+}
+console.log(chalk.green(score));
+
+
